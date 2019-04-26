25fdc9583b366ded3397c24b0e7989b7cf640073
RunaDahin <dahlin.runa@gmail.com>
2019-04-26 16:08:45 +0200

    Test kommentar

--- Slutprojekt/Slutprojekt/Program.cs
+++ Slutprojekt/Slutprojekt/Program.cs
@@ -20,5 +20,7 @@ namespace Slutprojekt
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
+
+        // Runas kommentar
     }
 }
