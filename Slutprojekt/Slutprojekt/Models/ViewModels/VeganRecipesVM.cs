6a3a57676ad19019c73efc10901ecc5da9e1a7cf
RunaDahin <dahlin.runa@gmail.com>
2019-04-29 14:30:42 +0200

    added Category list in VeganRecipesVM

---
+++ Slutprojekt/Slutprojekt/Models/ViewModels/VeganRecipesVM.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Slutprojekt.Models.ViewModels
+{
+    public class VeganRecipesVM
+    {
+        public List<string> Categories { get; set; } = new List<string>
+        {
+            "Breakfast",
+            "Lunch",
+            "Dinner",
+        };      
+
+    }
+}
