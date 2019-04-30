07943c0b0e642fe163b2a9fd6a64256f19bf2633
malinebbesen <malinebbesen@gmail.com>
2019-04-30 11:36:12 +0200

    fixat mer

--- Slutprojekt/SlutprojektDB/StepByStep.sql
+++ Slutprojekt/SlutprojektDB/StepByStep.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [rec].[StepByStep]
 (
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
-    [RecID] INT NOT NULL references rec.Recipe(ID), 
+    [RecID] INT NOT NULL references rec.Recipe(Id), 
     [Instruction] NVARCHAR(MAX) NOT NULL, 
     [SortOrder] INT NOT NULL
 )
