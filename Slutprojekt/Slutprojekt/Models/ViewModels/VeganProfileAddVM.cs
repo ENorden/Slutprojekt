f80eb0f90a4d53119c97e64ee9176cec15bde316
ENorden <elsanord@gmail.com>
2019-04-30 15:47:13 +0200

    Added view model add recipes

--- Slutprojekt/Slutprojekt/Models/ViewModels/VeganProfileAddVM.cs
+++ Slutprojekt/Slutprojekt/Models/ViewModels/VeganProfileAddVM.cs
@@ -7,5 +7,25 @@ namespace Slutprojekt.Models.ViewModels
 {
     public class VeganProfileAddVM
     {
+        public string Name { get; set; }
+        public List<string> Categories { get; set; }
+        public string Img { get; set; }
+        public List<Ingredient> Ingredients { get; set; }
+        public List<StepByStep> Steps { get; set; }
+
+
+        public class Ingredient
+        {
+            public string Name { get; set; }
+            public double Amount { get; set; }
+            public string Measure { get; set; }
+        }
+
+        public class StepByStep
+        {
+            public int SortOrder { get; set; }
+            public string Instruction { get; set; }
+        }
+
     }
 }
