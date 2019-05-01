8383752b265d74aeb72147535bbb7a3e268556c2
RunaDahin <dahlin.runa@gmail.com>
2019-05-01 18:04:13 +0200

    add recipe carousel

--- Slutprojekt/Slutprojekt/wwwroot/Scripts/JavaScript.js
+++ Slutprojekt/Slutprojekt/wwwroot/Scripts/JavaScript.js
@@ -1,1 +1,20 @@
-﻿console.log('test');
+﻿const buttons = document.querySelectorAll('button');
+const carouselItems = document.querySelectorAll('.carousel-item');
+
+console.log(buttons);
+
+
+console.log('hello from js file');
+
+
+buttons.forEach(button => button.addEventListener('click', (e) => {
+    carouselItems.forEach(item => {
+        item.classList.add('hide')
+    })
+
+    console.log(e.target.dataset.itemToShow);
+    document.querySelector(`#carousel-item-${e.target.dataset.itemToShow}`).classList.remove('hide')
+
+    console.log(button);
+
+}))
