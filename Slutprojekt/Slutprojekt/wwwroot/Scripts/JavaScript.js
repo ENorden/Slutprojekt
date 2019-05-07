c25175f28e594109b03a4bf325d82fb56a5edfce
malinebbesen <malinebbesen@gmail.com>
2019-05-07 15:16:56 +0200

    Formulär fixar

--- Slutprojekt/Slutprojekt/wwwroot/Scripts/JavaScript.js
+++ Slutprojekt/Slutprojekt/wwwroot/Scripts/JavaScript.js
@@ -1,12 +1,12 @@
 ﻿const buttons = document.querySelectorAll('button');
-const carouselItems = document.querySelectorAll('.carousel-item');
+const carouselItems = document.querySelectorAll('.RecipeLoop-item');
 
 buttons.forEach(button => button.addEventListener('click', (e) => {
     carouselItems.forEach(item => {
         item.classList.add('hide')
     })
 
-    document.querySelector(`#carousel-item-${e.target.dataset.itemToShow}`).classList.remove('hide')
+    document.querySelector(`#RecipeLoop-item-${e.target.dataset.itemToShow}`).classList.remove('hide')
 
 
 }))
